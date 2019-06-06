73bb36a40efbe815691e053fab474afaaaa8ea29
rwfazul <rwfazul@inf.ufsm.br>
2019-06-06 18:35:21 -0300

    add dict cursor'

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sys, os
 from flask import Flask, request, jsonify
 from flask_restful import reqparse, Api, Resource
 import psycopg2
+from psycopg2.extras import DictCursor
 
 
 # Flask init
@@ -30,7 +31,7 @@ class HomeRouter(Resource):
 class PlaylistsRouter(Resource):
 	def get(self, playlist_id=None):
 		try:
-			cur = conn.cursor()
+			cur = conn.cursor(cursor_factory=DictCursor)
 			if not playlist_id:
 				searchParser = reqparse.RequestParser()
 				searchParser.add_argument('s', type=str, required=False, location='args', help='Search query')
